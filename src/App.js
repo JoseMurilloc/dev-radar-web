8c4fcaa9274b49877125c6941f54467e2ba5cb5a
Jose Murillo <jooseemurillo@gmail.com>
2020-01-15 15:49:04 -0300

    estados criados e aplicado nos inputs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Componente: Bloco usolado de HTML, CSS e JS, no qual não interfere no restante da aplicação...
 // Propriedade: E como se fosse os atributos das tags HTML, para podemos enviar informarções pela {props} que são os argumentos de componente
@@ -10,6 +10,32 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [ github_username, setGithubUsername ] = useState('');
+  const [techs, setTechs ] = useState('');
+  const [ latitude, setLatitude] = useState('');
+  const [ longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      }, 
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
+  async function  handleAddDev(e) {
+    e.preventDefault();
+    
+  }
 
   return (
     <div id="app">
@@ -19,23 +45,23 @@ function App() {
           {/* Inputs com o contudos do JSON create POST Dev */}
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/> 
+            <input name="github_username" id="github_username" required value={github_username} onChange={e => setGithubUsername(e.target.value)} /> 
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/> 
+            <input name="techs" id="techs" required value={techs} onChange={e => setTechs(e.target.value)}/> 
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/> 
+              <input type="number" name="latitude" id="latitude" required value={latitude} onChange={e => setLatitude(e.target.value)}/> 
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/> 
+              <input type="number" name="longitude" id="longitude" required value={longitude} onChange={e => setLongitude(e.target.value)}/> 
             </div>
           </div>
 
