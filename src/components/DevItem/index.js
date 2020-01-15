42ade60faa2c617a09175975718be1c58c6aaf9c
Jose Murillo <jooseemurillo@gmail.com>
2020-01-15 16:50:50 -0300

    criar compoent DevItem

---
+++ src/components/DevItem/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './style.css';
+
+export default function DevItem({ dev }) {
+  return (
+    <li key={dev._id} className="dev-item">
+      <header>
+        <img src={dev.avatar_url} alt={dev.name}/>
+        <div className="user-info">
+          <strong>{dev.name}</strong>
+          <span>{dev.techs.map(tech =>(tech + ' '))}</span>
+        </div>
+      </header>
+      <p>{dev.bio}</p>
+      <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+    </li>
+  );
+}
