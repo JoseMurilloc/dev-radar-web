8e44fe2b20f496a5306ae93e9ccd318d01cd7f7d
Jose Murillo <jooseemurillo@gmail.com>
2020-01-15 10:55:30 -0300

    add css global + css main e app

--- src/App.js
+++ src/App.js
@@ -4,18 +4,48 @@ import React, { useState } from 'react';
 // Propriedade: E como se fosse os atributos das tags HTML, para podemos enviar informarções pela {props} que são os argumentos de componente
 // Estado: São as informaçaões mantida pelos componentes(Lembrar: imutabilidade )
 
-function App() {
-  const [counter, setCounter] = useState(0);
+import './global.css';
+import './App.css';
+import './Sidebar.css';
 
-  function incrementCount() {
-    setCounter(counter + 1);
-  }
+function App() {
 
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCount}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          {/* Inputs com o contudos do JSON create POST Dev */}
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required/> 
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/> 
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/> 
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/> 
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
