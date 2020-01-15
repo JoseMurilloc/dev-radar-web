2ffa3f3b6a2a0c67f3966faec5751f6a6e170d60
Jose Murillo <jooseemurillo@gmail.com>
2020-01-15 09:41:42 -0300

    Header exemplo de props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+// Componente: Bloco usolado de HTML, CSS e JS, no qual não interfere no restante da aplicação...
+// Propriedade: E como se fosse os atributos das tags HTML, para podemos enviar informarções pela {props} que são os argumentos de componente
+
+import Header from './Header';
+
 function App() {
-  return <h1>Hello world</h1>
+  return (
+    <Header title="Dashboard" />
+  );
 }
 
 export default App;
