9c076fd900c03f1ae7af109aea7e5832ca99816b
Jose Murillo <jooseemurillo@gmail.com>
2020-01-15 11:12:49 -0300

    add estrutura do main no App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 // Componente: Bloco usolado de HTML, CSS e JS, no qual não interfere no restante da aplicação...
 // Propriedade: E como se fosse os atributos das tags HTML, para podemos enviar informarções pela {props} que são os argumentos de componente
@@ -7,6 +7,7 @@ import React, { useState } from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
 
@@ -43,7 +44,57 @@ function App() {
         </form>
       </aside>
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/43470555?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>José Murillo</strong>
+                <span>NodeJS, ReactJS</span>
+              </div>
+            </header>
+            <p>#nodejs</p>
+            <a href="https://github.com/JoseMurilloc">Acessar perfil no Github</a>
+          </li>
 
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/43470555?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>José Murillo</strong>
+                <span>NodeJS, ReactJS</span>
+              </div>
+            </header>
+            <p>#nodejs</p>
+            <a href="https://github.com/JoseMurilloc">Acessar perfil no Github</a>
+          </li>
+
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/43470555?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>José Murillo</strong>
+                <span>NodeJS, ReactJS</span>
+              </div>
+            </header>
+            <p>#nodejs</p>
+            <a href="https://github.com/JoseMurilloc">Acessar perfil no Github</a>
+          </li>
+
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/43470555?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>José Murillo</strong>
+                <span>NodeJS, ReactJS</span>
+              </div>
+            </header>
+            <p>#nodejs</p>
+            <a href="https://github.com/JoseMurilloc">Acessar perfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
