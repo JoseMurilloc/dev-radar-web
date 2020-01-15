2fabfa56b89fea4ee5e300672077fe34441fb84a
Jose Murillo <jooseemurillo@gmail.com>
2020-01-15 10:01:00 -0300

    exemplo contador

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Componente: Bloco usolado de HTML, CSS e JS, no qual não interfere no restante da aplicação...
 // Propriedade: E como se fosse os atributos das tags HTML, para podemos enviar informarções pela {props} que são os argumentos de componente
-
-import Header from './Header';
+// Estado: São as informaçaões mantida pelos componentes(Lembrar: imutabilidade )
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCount() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <Header title="Dashboard" />
+    <>
+      <h1>Contador: {counter}</h1>
+      <button onClick={incrementCount}>Incrementar</button>
+    </>
   );
 }
 
--- src/Header.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function Header(props) {
-  console.log(props);
-  return <h1>{props.title}</h1>
-}
